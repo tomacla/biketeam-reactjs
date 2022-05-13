5698a1e1a213032fc86003327290427ceb296823
Loan <laubergeon@gmail.com>
2022-05-13 17:51:09 +0200

    add link

--- src/components/Team/Event.tsx
+++ src/components/Team/Event.tsx
@@ -1,10 +1,13 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { FC, memo, ReactNode } from 'react';
-import { Badge, Button, Card } from 'react-bootstrap';
+import { Badge, Button, Card, Col, Row } from 'react-bootstrap';
 import { TeamEvent, EventType } from '../../redux/interfaces';
 import styled from 'styled-components';
 import moment from 'moment';
 import 'moment/locale/fr';
 import { API_URL } from '../common/constants';
+import { useSref } from '@uirouter/react';
+import { LinkProps } from '@uirouter/react/lib/hooks/useSref';
 
 const EventCard = styled(Card)`
   margin: 8px 0 8px 0;
@@ -16,9 +19,14 @@ const SeeIcon = styled.i.attrs({
 margin-right: 4px;
 `;
 
-const BikeIcon = styled.i.attrs({
-  className: 'bi bi-bicycle'
-})`
+
+interface BikeIconProps {
+  className: string;
+}
+
+const BikeIcon = styled.i.attrs<BikeIconProps>((({ className }) => ({
+  className: `bi bi-${className}`
+})))`
 margin: 0 4px 0 4px;
 `;
 
@@ -27,14 +35,27 @@ font-size: .875em;
 color: #6c757d!important;
 `;
 
-const GroupBadge = styled(Badge)`
+interface GroupBadgeProps {
+  color?: string;
+}
+
+const GroupBadge = styled(Badge) <GroupBadgeProps>`
 margin-right: 4px;
+background-color: ${({ color }) => (color)} !important;
 `;
 
-const EventImage = styled.img`
-width: 100%;
-height: auto;
+const EventImage = styled.img.attrs({
+  className: 'mx-auto d-block shadow rounded w-100 h-auto mx-auto'
+})`
 margin-bottom: 8px;
+`;
+
+const ImageContainer = styled(Row)`
+  justify-content: center;
+`;
+
+const EventContent = styled.p`
+  white-space: pre-wrap;
 `;
 
 interface EventProps {
@@ -43,39 +64,75 @@ interface EventProps {
 
 function toTypeTitle(type: EventType): ReactNode {
   return {
-    TRIP: <>Trip</>,
-    RIDE: <>Ride<BikeIcon /></>,
-    PUBLICATION: <>Publication</>
+    TRIP: <>Trip<BikeIcon className='signpost-2' /></>,
+    RIDE: <>Ride<BikeIcon className='bicycle' /></>,
+    PUBLICATION: <>Publication<BikeIcon className='newspaper' /></>
   }[type]
 }
 
-function toImage(type: EventType, teamId: string, id: string): ReactNode {
+function toEventImage(type: EventType, teamId: string, id: string, link?: LinkProps): ReactNode {
   return {
-    TRIP: <EventImage alt='image du trip' src={`${API_URL}/${teamId}/trips/${id}/image`} />,
-    RIDE: <EventImage alt='image du ride' src={`${API_URL}/${teamId}/rides/${id}/image`} />,
+    TRIP: <a {...link}><EventImage alt='image du trip' src={`${API_URL}/${teamId}/trips/${id}/image`} /></a>,
+    RIDE: <a {...link}><EventImage alt='image du ride' src={`${API_URL}/${teamId}/rides/${id}/image`} /></a>,
     PUBLICATION: <EventImage alt='image de la publication' src={`${API_URL}/${teamId}/publications/${id}/image`} />
   }[type]
 }
 
+function toBadgeColor(badge: string): string | undefined {
+  return {
+    'G1': 'red',
+    'G2': '#ff5324',
+    'G3': '#eeb31e',
+    'GRAVEL': 'chocolate',
+    'CHILL': 'green',
+  }[badge.toUpperCase()]
+}
+
+function toEventTitle(type: EventType, title: string, date?: Date): string {
+  return {
+    TRIP: title + ' - ' + moment(date).format('LL'),
+    RIDE: title + ' - ' + moment(date).format('LL'),
+    PUBLICATION: title
+  }[type]
+}
+
+function useEventLink(type: EventType, id: string): LinkProps | undefined {
+  return {
+    TRIP: useSref('trip', { tripId: id }),
+    RIDE: useSref('ride', { rideId: id }),
+    PUBLICATION: undefined
+  }[type]
+}
+
 const Event: FC<EventProps> = ({ event }) => {
+  const { type, publishedAt, title, date, badges, content, imaged, teamId, id } = event;
+  const goToEvent = useEventLink(type, id);
   return (
     <EventCard>
-      <CardHeader className='text-end'>{toTypeTitle(event.type)} publié {moment(event.publishedAt).locale('fr').fromNow()}</CardHeader>
+      <CardHeader className='text-end'>{toTypeTitle(type)}publié {moment(publishedAt).locale('fr').fromNow()}</CardHeader>
       <Card.Body>
-        <Card.Title>{event.title} - {moment(event.date).format('L')}</Card.Title>
+        <Card.Title>{toEventTitle(type, title, date)}</Card.Title>
         <Card.Text>
           <p>
-            {event.badges.map((badge) => <GroupBadge key={badge} bg="secondary">{badge}</GroupBadge>)}
+            {badges.map((badge) => <GroupBadge color={toBadgeColor(badge)} key={badge} bg="secondary">{badge}</GroupBadge>)}
           </p>
-          <p>
-            {event.content}
-          </p>
-          {event.imaged ? (toImage(event.type, event.teamId, event.id)) : (null)}
+          <EventContent>
+            {content}
+          </EventContent>
+          {imaged ? (
+            <ImageContainer>
+              <Col xs={12} md={6} >
+                {toEventImage(type, teamId, id, goToEvent)}
+              </Col>
+            </ImageContainer>
+          ) : (null)}
         </Card.Text>
       </Card.Body>
-      <Card.Footer className='text-center'>
-        <Button size="sm" variant="secondary"><SeeIcon />Voir</Button>
-      </Card.Footer>
+      {
+        type !== 'PUBLICATION' ? (<Card.Footer className='text-center'>
+          <Button {...goToEvent} size="sm" variant="secondary"><SeeIcon />Voir</Button>
+        </Card.Footer>) : null
+      }
     </EventCard>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -10,16 +10,20 @@ import { pushStateLocationPlugin, UIRouter } from '@uirouter/react';
 import Home from './components/Views/Home';
 import Teams from './components/Views/Teams';
 import HomeTeam from './components/Views/HomeTeam';
+import Ride from './components/Views/Ride';
+import Trip from './components/Views/Trip';
 
 const homeState = { name: 'home', url: '/', component: Home };
 const teamsState = { name: 'teams', url: '/teams', component: Teams };
 const homeTeamState = { name: 'homeTeam', url: '/{teamId:string}', component: HomeTeam };
+const rideState = { name: 'ride', url: '/rides/{rideId:string}', component: Ride };
+const tripState = { name: 'trip', url: '/trips/{tripId:string}', component: Trip };
 
 ReactDOM.render(
   <React.StrictMode>
     <StoreProvider>
       <UIRouter plugins={[pushStateLocationPlugin]}
-        states={[homeState, teamsState, homeTeamState]} >
+        states={[homeState, teamsState, homeTeamState, rideState, tripState]} >
         <App />
       </UIRouter>
     </StoreProvider >
